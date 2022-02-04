dee99f8f44fa25c8c3898b129762ebe3466dd2cd
Steve Ernstberger <steve32285@gmail.com>
2022-02-04 14:01:57 -0500

    created rating component

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -3,23 +3,15 @@ import {
   Avatar,
   Box,
   Button,
-  IconButton,
   Stack,
   Typography,
-  ToggleButton,
   Collapse,
 } from "@mui/material";
-import {
-  ArrowDropDown,
-  ArrowDropUp,
-  ThumbDownAlt,
-  ThumbDownOffAlt,
-  ThumbUpAlt,
-  ThumbUpOffAlt,
-} from "@mui/icons-material";
+import { ArrowDropDown, ArrowDropUp } from "@mui/icons-material";
 import { timeAgo } from "../../utils";
 import { CommentProps } from "./types";
 import ReplyForm from "./ReplyForm";
+import Rating from "../Rating";
 
 export interface CommentComponentProps extends CommentProps {
   upvoteEnabled?: boolean;
@@ -41,67 +33,39 @@ const Comment = ({
   const [showReplies, setShowReplies] = useState<boolean>(false);
 
   return (
-    <div>
-      <Stack direction="row" spacing={2}>
-        <Avatar src={user.avatar} />
-        <div>
-          <Typography>
-            <strong>{user.name}</strong> {timeAgo(createDate)}
-          </Typography>
-          <Typography>{comment}</Typography>
-          <Stack direction="row">
-            {upvoteEnabled && (
-              <Stack direction="row">
-                <IconButton
-                  value="check"
-                  component={ToggleButton}
-                  selected={rating === "like"}
-                  onChange={() => {
-                    setRating(rating === "like" ? null : "like");
-                  }}
-                >
-                  {rating === "like" ? <ThumbUpAlt /> : <ThumbUpOffAlt />}
-                </IconButton>
-                <Typography>511</Typography>
-              </Stack>
-            )}
-            {downvoteEnabled && (
-              <IconButton
-                value="check"
-                component={ToggleButton}
-                selected={rating === "dislike"}
-                onChange={() => {
-                  setRating(rating === "dislike" ? null : "dislike");
-                }}
-              >
-                {rating === "dislike" ? <ThumbDownAlt /> : <ThumbDownOffAlt />}
-              </IconButton>
-            )}
-            <Button onClick={() => setReplying(true)} disabled={replying}>
-              Reply
-            </Button>
-          </Stack>
-          <ReplyForm id={1} open={replying} setOpen={setReplying} />
-        </div>
-      </Stack>
-      {replies && replies?.length > 0 && (
-        <>
-          <Button
-            startIcon={showReplies ? <ArrowDropUp /> : <ArrowDropDown />}
-            onClick={() => setShowReplies(!showReplies)}
-          >
-            {showReplies ? "Hide" : "Show"} {replies.length} replies
+    <Stack direction="row" spacing={2}>
+      <Avatar src={user.avatar} />
+      <div>
+        <Typography>
+          <strong>{user.name}</strong> {timeAgo(createDate)}
+        </Typography>
+        <Typography>{comment}</Typography>
+        <Stack direction="row">
+          <Rating {...{ upvoteEnabled, downvoteEnabled, rating, setRating }} />
+          <Button onClick={() => setReplying(true)} disabled={replying}>
+            Reply
           </Button>
-          <Collapse in={showReplies}>
-            <Box marginLeft={7}>
-              {replies.map((reply: CommentProps) => {
-                return <Comment {...reply} />;
-              })}
-            </Box>
-          </Collapse>
-        </>
-      )}
-    </div>
+        </Stack>
+        <ReplyForm id={1} open={replying} setOpen={setReplying} />
+        {replies && replies?.length > 0 && (
+          <>
+            <Button
+              startIcon={showReplies ? <ArrowDropUp /> : <ArrowDropDown />}
+              onClick={() => setShowReplies(!showReplies)}
+            >
+              {showReplies ? "Hide" : "Show"} {replies.length} replies
+            </Button>
+            <Collapse in={showReplies}>
+              <Box marginBottom={3}>
+                {replies.map((reply: CommentProps) => {
+                  return <Comment {...reply} />;
+                })}
+              </Box>
+            </Collapse>
+          </>
+        )}
+      </div>
+    </Stack>
   );
 };
 
---
+++ src/components/Rating/index.tsx
@@ -0,0 +1,55 @@
+import { IconButton, Stack, Typography, ToggleButton } from "@mui/material";
+import {
+  ThumbDownAlt,
+  ThumbDownOffAlt,
+  ThumbUpAlt,
+  ThumbUpOffAlt,
+} from "@mui/icons-material";
+
+export interface RatingProps {
+  upvoteEnabled?: boolean;
+  downvoteEnabled?: boolean;
+  rating: "like" | "dislike" | null;
+  setRating: (rating: "like" | "dislike" | null) => void;
+}
+
+const Rating = ({
+  upvoteEnabled = false,
+  downvoteEnabled = false,
+  rating,
+  setRating,
+}: RatingProps) => {
+  return (
+    <Stack direction="row">
+      {upvoteEnabled && (
+        <Stack direction="row">
+          <IconButton
+            value="check"
+            component={ToggleButton}
+            selected={rating === "like"}
+            onChange={() => {
+              setRating(rating === "like" ? null : "like");
+            }}
+          >
+            {rating === "like" ? <ThumbUpAlt /> : <ThumbUpOffAlt />}
+          </IconButton>
+          <Typography>511</Typography>
+        </Stack>
+      )}
+      {downvoteEnabled && (
+        <IconButton
+          value="check"
+          component={ToggleButton}
+          selected={rating === "dislike"}
+          onChange={() => {
+            setRating(rating === "dislike" ? null : "dislike");
+          }}
+        >
+          {rating === "dislike" ? <ThumbDownAlt /> : <ThumbDownOffAlt />}
+        </IconButton>
+      )}
+    </Stack>
+  );
+};
+
+export default Rating;
